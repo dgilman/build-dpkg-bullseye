2892934a5e2875d5281a854dddbee3d71ae6193f
David Gilman <davidgilman1@gmail.com>
2021-06-16 22:42:59 -0400

    git-buildpackage support

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -
 # Install build dependencies automatically
 mk-build-deps --install --tool="${install_tool}" debian/control
 # Build the package
-dpkg-buildpackage $@
+gbp buildpackage
 # Output the filename
 cd ..
 filename=`ls *.deb | grep -v -- -dbgsym`
